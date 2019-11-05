de47fa3d0593545049363928c97ff377df3d9cb5
MinersWin <minerswin@minerswin.de>
2019-11-05 11:44:26 +0100

    Blocking more Windows Servers

---
+++ Scripts/Batch/Blocking/BlockingMoreWindowsServers.bat
@@ -0,0 +1,225 @@
+::TuningPack 4.0 by MinersWin
+::05.11.2019
+::https://tuning-pack.de
+copy "%WINDIR%\system32\drivers\etc\hosts" "%WINDIR%\system32\drivers\etc\hosts.bak" > nul 2>&1
+attrib -r "%WINDIR%\system32\drivers\etc\hosts" > nul 2>&1
+find /C /I "184-86-53-99.deploy.static.akamaitechnologies.com" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 184-86-53-99.deploy.static.akamaitechnologies.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "a.ads1.msn.com" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 a.ads1.msn.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "a.ads2.msads.net" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 a.ads2.msads.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "a.ads2.msn.com" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 a.ads2.msn.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "a.rad.msn.com" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 a.rad.msn.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "a-0001.a-msedge.net" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 a-0001.a-msedge.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "a-0002.a-msedge.net" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 a-0002.a-msedge.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "a-0003.a-msedge.net" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 a-0003.a-msedge.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "a-0004.a-msedge.net" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 a-0004.a-msedge.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "a-0005.a-msedge.net" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 a-0005.a-msedge.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "a-0006.a-msedge.net" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 a-0006.a-msedge.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "a-0007.a-msedge.net" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 a-0007.a-msedge.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "a-0008.a-msedge.net" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 a-0008.a-msedge.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "a-0009.a-msedge.net" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 a-0009.a-msedge.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "a1621.g.akamai.net" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 a1621.g.akamai.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "a1856.g2.akamai.net" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 a1856.g2.akamai.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "a1961.g.akamai.net" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 a1961.g.akamai.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "a978.i6g1.akamai.net" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 a978.i6g1.akamai.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "ac3.msn.com" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 ac3.msn.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "ad.doubleclick.net" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 ad.doubleclick.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "adnexus.net" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 adnexus.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "adnxs.com" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 adnxs.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "ads.msn.com" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 ads.msn.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "ads1.msads.net" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 ads1.msads.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "ads1.msn.com" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 ads1.msn.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "aidps.atdmt.com" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 aidps.atdmt.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "aka-cdn-ns.adtech.de" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 aka-cdn-ns.adtech.de>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "a-msedge.net" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 a-msedge.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "any.edge.bing.com" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 any.edge.bing.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "az361816.vo.msecnd.net" %WINDIR%\system32\drivers\etc\hosts	
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 az361816.vo.msecnd.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "az512334.vo.msecnd.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 az512334.vo.msecnd.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "b.ads1.msn.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 b.ads1.msn.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "b.ads2.msads.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 b.ads2.msads.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "b.rad.msn.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 b.rad.msn.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "bingads.microsoft.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 bingads.microsoft.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "bs.serving-sys.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 bs.serving-sys.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "c.atdmt.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 c.atdmt.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "cdn.atdmt.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 cdn.atdmt.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "cds26.ams9.msecn.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 cds26.ams9.msecn.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "compatexchange.cloudapp.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 compatexchange.cloudapp.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "corp.sts.microsoft.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 corp.sts.microsoft.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "corpext.msitadfs.glbdns2.microsoft.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 corpext.msitadfs.glbdns2.microsoft.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "cs1.wpc.v0cdn.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 cs1.wpc.v0cdn.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "cy2.vortex.data.microsoft.com.akadns.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 cy2.vortex.data.microsoft.com.akadns.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "db3aqu.atdmt.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 db3aqu.atdmt.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "diagnostics.support.microsoft.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 diagnostics.support.microsoft.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "e2835.dspb.akamaiedge.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 e2835.dspb.akamaiedge.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "e7341.g.akamaiedge.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 e7341.g.akamaiedge.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "e7502.ce.akamaiedge.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 e7502.ce.akamaiedge.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "e8218.ce.akamaiedge.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 e8218.ce.akamaiedge.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "ec.atdmt.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 ec.atdmt.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "fe2.update.microsoft.com.akadns.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 fe2.update.microsoft.com.akadns.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "fe2.update.microsoft.com.akadns.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 fe2.update.microsoft.com.akadns.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "feedback.microsoft-hohm.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 feedback.microsoft-hohm.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "feedback.search.microsoft.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 feedback.search.microsoft.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "feedback.windows.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 feedback.windows.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "flex.msn.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 flex.msn.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "g.msn.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 g.msn.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "h1.msn.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 h1.msn.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "h2.msn.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 h2.msn.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "hostedocsp.globalsign.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 hostedocsp.globalsign.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "i1.services.social.microsoft.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 i1.services.social.microsoft.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "i1.services.social.microsoft.com.nsatc.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 i1.services.social.microsoft.com.nsatc.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "ipv6.msftncsi.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 ipv6.msftncsi.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "ipv6.msftncsi.com.edgesuite.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 ipv6.msftncsi.com.edgesuite.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "lb1.www.ms.akadns.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 lb1.www.ms.akadns.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "live.rads.msn.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 live.rads.msn.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "m.adnxs.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 m.adnxs.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "m.hotmail.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 m.hotmail.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "msedge.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 msedge.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "msftncsi.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 msftncsi.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "msnbot-65-55-108-23.search.msn.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 msnbot-65-55-108-23.search.msn.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "msntest.serving-sys.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 msntest.serving-sys.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "onesettings-db5.metron.live.nsatc.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 onesettings-db5.metron.live.nsatc.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "pre.footprintpredict.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 pre.footprintpredict.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "preview.msn.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 preview.msn.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "rad.live.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 rad.live.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "rad.msn.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 rad.msn.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "s0.2mdn.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 s0.2mdn.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "schemas.microsoft.akadns.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 schemas.microsoft.akadns.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "secure.adnxs.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 secure.adnxs.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "secure.flashtalking.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 secure.flashtalking.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "settings-win.data.microsoft.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 settings-win.data.microsoft.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "sls.update.microsoft.com.akadns.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 sls.update.microsoft.com.akadns.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "ssw.live.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 ssw.live.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "static.2mdn.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 static.2mdn.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "statsfe1.ws.microsoft.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 statsfe1.ws.microsoft.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "statsfe2.update.microsoft.com.akadns.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 statsfe2.update.microsoft.com.akadns.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "statsfe2.update.microsoft.com.akadns.net," %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 statsfe2.update.microsoft.com.akadns.net,>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "statsfe2.ws.microsoft.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 statsfe2.ws.microsoft.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "survey.watson.microsoft.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 survey.watson.microsoft.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "survey.watson.microsoft.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 survey.watson.microsoft.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "view.atdmt.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 view.atdmt.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "vortex-bn2.metron.live.com.nsatc.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 vortex-bn2.metron.live.com.nsatc.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "vortex-cy2.metron.live.com.nsatc.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 vortex-cy2.metron.live.com.nsatc.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "watson.live.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 watson.live.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "watson.microsoft.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 watson.microsoft.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "watson.telemetry.microsoft.com.nsatc.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 watson.telemetry.microsoft.com.nsatc.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "wes.df.telemetry.microsoft.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 wes.df.telemetry.microsoft.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "win10.ipv6.microsoft.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 win10.ipv6.microsoft.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "www.bingads.microsoft.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 www.bingads.microsoft.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "www.go.microsoft.akadns.net" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 www.go.microsoft.akadns.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "www.msftncsi.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 www.msftncsi.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "a248.e.akamai.net" %WINDIR%\system32\drivers\etc\hosts
+rem skype & itunes issues 
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 a248.e.akamai.net>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "apps.skype.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 apps.skype.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "c.msn.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 c.msn.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "pricelist.skype.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 pricelist.skype.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "s.gateway.messenger.live.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 s.gateway.messenger.live.com>>%WINDIR%\system32\drivers\etc\hosts
+find /C /I "ui.skype.com" %WINDIR%\system32\drivers\etc\hosts
+if %ERRORLEVEL% NEQ 0 echo ^0.0.0.0 ui.skype.com>>%WINDIR%\system32\drivers\etc\hosts
+attrib +r "%WINDIR%\system32\drivers\etc\hosts" > nul 2>&1
