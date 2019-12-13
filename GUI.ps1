471fc5e2aaa859a8ee13acf9a1208bc1ea6fff45
MinersWin <minerswin@minerswin.de>
2019-12-13 23:53:01 +0100

    Template Overhaul

--- GUI.ps1
+++ GUI.ps1
@@ -305,12 +305,15 @@ $ComboBox1.Items.Add("Best Performance")
 $ComboBox1.Items.Add("Maximum Battery Life")
 $ComboBox1.Items.Add("Maximum Privacy")
 $ComboBox1.Items.Add("Recommended")
+$ComboBox1.Items.Add("Clear")
+$ComboBox1.Items.Add("ULTIMATE PERFORMANCE")
 $ComboBox1.SelectedItem = "Recommended"
 $ComboBox2.Items.Add("Best Performance")
 $ComboBox2.Items.Add("Maximum Battery Life")
 $ComboBox2.Items.Add("Maximum Privacy")
 $ComboBox2.Items.Add("Recommended")
-$ComboBox2.SelectedItem = "Recommended"
+$ComboBox1.Items.Add("ULTIMATE PERFORMANCE")
+$ComboBox2.SelectedItem = $Config.Application.Standart_Template
 
 #Progressbar Reset
 $ProgressBar1.Value = 0
@@ -1115,73 +1118,22 @@ $Button2.Add_Click{(Apply-Template)}
 
 function Apply-Template{##############FIXME##############
     if ($ComboBox1.SelectedItem -eq "Recommended"){
-        $CheckBox1.Checked = $true 
-        $CheckBox2.Checked = $true
-        $CheckBox3.Checked = $true
-        $CheckBox4.Checked = $true
-        $CheckBox5.Checked = $true
-        $CheckBox6.Checked = $false
-        $CheckBox7.Checked = $true
-        $CheckBox8.Checked = $true
-        $CheckBox9.Checked = $true
-        $CheckBox10.Checked = $true
-        $CheckBox11.Checked = $true
-        $CheckBox12.Checked = $true
-        $CheckBox13.Checked = $true
-        $CheckBox14.Checked = $true
-        $CheckBox15.Checked = $true
-        $CheckBox16.Checked = $true
-        $CheckBox17.Checked = $true
-        $CheckBox18.Checked = $true
-        $CheckBox19.Checked = $true
-        $CheckBox20.Checked = $true
-        $CheckBox21.Checked = $true
-        $CheckBox22.Checked = $true
-        $CheckBox23.Checked = $true
-        $CheckBox24.Checked = $true
-        $CheckBox25.Checked = $true
-        $CheckBox26.Checked = $true
-        $CheckBox27.Checked = $true
-        $CheckBox29.Checked = $true
-        $CheckBox30.Checked = $true
-        $CheckBox31.Checked = $false
-        $CheckBox32.Checked = $true
-        $CheckBox33.Checked = $true
-        $CheckBox34.Checked = $true
-        $CheckBox35.Checked = $true
-        $CheckBox36.Checked = $true
-        $CheckBox37.Checked = $true
-        $CheckBox38.Checked = $true
-        $CheckBox28.Checked = $true
-        $CheckBox39.Checked = $true
-        $CheckBox40.Checked = $true
-        $CheckBox41.Checked = $false
-        $CheckBox42.Checked = $true
-        $CheckBox43.Checked = $true
-        $CheckBox44.Checked = $false
-        $CheckBox45.Checked = $true
-        $CheckBox46.Checked = $false
-        $CheckBox47.Checked = $true
-        $CheckBox48.Checked = $true
-        $CheckBox49.Checked = $true
-        $CheckBox50.Checked = $false
-        $CheckBox51.Checked = $true
-        $CheckBox52.Checked = $true
-        $CheckBox53.Checked = $false
-        $CheckBox54.Checked = $true
-        $CheckBox55.Checked = $true
-        $CheckBox57.Checked = $true
-        $CheckBox58.Checked = $true
-        $CheckBox60.Checked = $true
-        $CheckBox64.Checked = $true
-        $CheckBox65.Checked = $true
-        $CheckBox66.Checked = $true
-        $CheckBox69.Checked = $true
-        $CheckBox70.Checked = $true
-        $CheckBox71.Checked = $true
-        $CheckBox72.Checked = $true
-        $CheckBox74.Checked = $true
-        $CheckBox75.Checked = $true
+        .\Templates\Recommended.ps1
+    }
+    if ($ComboBox1.SelectedItem -eq "Best Performance"){
+        .\Templates\BestPerformance.ps1
+    }
+    if ($ComboBox1.SelectedItem -eq "Maximum Battery Life"){
+        .\Templates\SaveEnergy.ps1
+    }
+    if ($ComboBox1.SelectedItem -eq "Maximum Privacy"){
+        .\Templates\Privacy.ps1
+    }
+    if ($ComboBox1.SelectedItem -eq "ULTIMATE PERFORMACE"){
+        .\Templates\UltimatePerformance.ps1
+    }
+    if ($ComboBox1.SelectedItem -eq "Clear"){
+        .\Templates\Clear.ps1
     }
 }
 
