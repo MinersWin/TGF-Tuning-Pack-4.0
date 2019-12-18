b146814c9d8a720c4bf25f45acb28105f859b27c
MinersWin <minerswin@minerswin.de>
2019-12-19 00:11:21 +0100

    Create Start.bat

---
+++ Start.bat
@@ -0,0 +1,5 @@
+::TuningPack 4.0 by MinersWin
+::19.12.2019
+::https://tuning-pack.de
+
+PowerShell.exe -Command "& {Start-Process PowerShell.exe -ArgumentList '-ExecutionPolicy Bypass -File "GUI.ps1"' -Verb RunAs}"
