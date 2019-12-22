a89c8216a8fe33fdcf90f409d14a7441cd71a798
MinersWin <minerswin@minerswin.de>
2019-12-22 21:11:38 +0100

    Just Fixing

--- Start.bat
+++ Start.bat
@@ -2,4 +2,6 @@
 ::19.12.2019
 ::https://tuning-pack.de
 
-PowerShell.exe -Command "& {Start-Process PowerShell.exe -ArgumentList '-ExecutionPolicy Bypass -File "GUI.ps1"' -Verb RunAs}"
+@ECHO OFF
+PowerShell.exe -Command "& './GUI.ps1'"
+PAUSE
