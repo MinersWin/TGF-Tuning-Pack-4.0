e4f66a6b57fe43b35c28877150800deb7376fe46
MinersWin <minerswin@minerswin.de>
2020-01-05 19:05:47 +0100

    Added Templates

---
+++ Templates/SaveEnergy.ps1
@@ -0,0 +1,125 @@
+#Tuning Pack 4.0 SaveEnergy Template
+#TGF Tuning Pack 4.0 by MinersWin
+#https://tuning-pack.de
+#14.12.2019
+
+#Registry Tweaks
+$CheckBox1.Checked = $true              #Show Computer Shortcut on Desktop
+$CheckBox2.Checked = $true              #Show Network Shortcut on Desktop
+$CheckBox3.Checked = $true              #Classic Vertical Icon Spacing
+$CheckBox4.Checked = $true              #Lock the Taskbar
+$CheckBox5.Checked = $true              #Always show all icons on the Taskbar
+$CheckBox6.Checked = $false             #Delay taskbar thumbnail pop-ups to 10 seconds
+$CheckBox7.Checked = $true              #Enable Classic Control Panel View
+$CheckBox8.Checked = $true              #Turn Off Sticky Keys when SHIFT is pressed 5 Times
+$CheckBox9.Checked = $true              #Turn OFF Filter Keys when SHIFT is pressed for 8 seconds
+$CheckBox10.Checked = $true             #Disable Hibernation
+$CheckBox11.Checked = $true             #Underline Keyboard shortcuts and access Keys
+$CheckBox12.Checked = $true             #Show Known file extensions in Explorer
+$CheckBox13.Checked = $true             #Hide indication for compressed NTFS files
+$CheckBox14.Checked = $true             #Show Hidden Files in Explorer
+$CheckBox15.Checked = $true             #Show Super Hidden Files in Explorer
+$CheckBox16.Checked = $true             #Prevent both Windows and Office from creating LNK files in the Recents folder
+$CheckBox17.Checked = $true             #Replace Utilman with CMD
+$CheckBox18.Checked = $false             #Add the Option 'Processor performance core parking min. cores'
+$CheckBox19.Checked = $false             #Disable CPU Core Parking
+$CheckBox20.Checked = $true             #Remove Logon screen wallpaper/background Will use solid color instead (Accent color)
+$CheckBox21.Checked = $true             #Disable Lockscreen
+$CheckBox22.Checked = $true             #Remover Versioning Tab from properties
+$CheckBox23.Checked = $true             #Disable Jump Lists
+$CheckBox24.Checked = $true             #Disable Windows Error Reporting
+$CheckBox25.Checked = $true             #Disable Cortana (Speech, Search Assistand, which also sends information to Microsoft)
+$CheckBox26.Checked = $true             #Hide search box from Taskbar., You can still search by pressing the Win key and start typing what youre looking for
+$CheckBox27.Checked = $true             #Disable MRU lists (jump lists) of XAML apps in Start Menu
+$CheckBox29.Checked = $true             #Disable Disk Quota tab, which appears as a tab when right-clicking on drive letter - properties
+$CheckBox30.Checked = $true             #Disable creation of an Advertising ID
+$CheckBox31.Checked = $false            #Remove Pin to start
+$CheckBox32.Checked = $true             #Disable Cortana, Bing Search and Searchbar
+$CheckBox33.Checked = $true             #Turn off the Error Dialog
+$CheckBox34.Checked = $true             #Disable Administrative Shares
+$CheckBox35.Checked = $true             #Add "Reboot to Recovery" to right-click menu of "This PC"
+$CheckBox36.Checked = $true             #Change Clock and Date formats of current user to 24H, metric
+$CheckBox37.Checked = $true             #Enable Developer Mode
+$CheckBox38.Checked = $true             #Remove telemetry and data collection
+$CheckBox28.Checked = $true             #Set Windows Explorer to start on 'This PC' instad of 'Quick Access'
+#Remove Services
+$CheckBox39.Checked = $true             #Disable Connected USer Experience and Telemetry (To turn off Telemetry and Data Collection)
+$CheckBox40.Checked = $true             #Disable Diagnostic Policy Service
+$CheckBox41.Checked = $false            #Disable Distributed Link Tracking Client (If your Computer ios not connected to any network)
+$CheckBox42.Checked = $true             #Disable WAP Push Message Routing Service (To turn off Telemetry and Data Collection)
+$CheckBox43.Checked = $true             #Disable Downloaded Maps Manager (If you dont use Maps app)
+$CheckBox44.Checked = $false            #Disable IP Helper (If you dont use IPv6 connection)
+$CheckBox45.Checked = $true             #Disable Programm Compatibility Assistant Service
+$CheckBox46.Checked = $false            #Disable Print Spooler (If you dont have a printer)
+$CheckBox47.Checked = $true             #Disable Remote Registry (You can set it to DISABLED for Security purposes)
+$CheckBox48.Checked = $true             #Disable Secondary Logon
+$CheckBox49.Checked = $true             #Disable Security Center
+$CheckBox50.Checked = $false            #Disable TCP/IP NetBIOS Helper (If you are not in a workgroup network)
+$CheckBox51.Checked = $true             #Disable Touch Keyboard and Handwriting Panel Service (If you dont want to use touch keyboard and handwriting features)
+$CheckBox52.Checked = $true             #Disable Windows Error Reporting Service
+$CheckBox53.Checked = $false            #Disable Windows Image Acquisition (WIA) (If you dont have a scanner)
+$CheckBox54.Checked = $true             #Disable Windows Search
+$CheckBox55.Checked = $true             #Disable tracking Services
+$CheckBox57.Checked = $true             #Disable XBox Services
+$CheckBox58.Checked = $true             #Disable AllJoyn Router Service
+$checkBox59.Checked = $false            #Disable Bluetooth Services
+$CheckBox60.Checked = $true             #Disable Geolocation Service
+$CheckBox64.Checked = $true             #Disable Phone Service
+$CheckBox65.Checked = $true             #Disable Windows Biometric Service
+$CheckBox66.Checked = $true             #Disable Windows Mobile Hotspot Service
+$CheckBox67.Checked = $true             #Disable Windows Media Player Network Sharing Service
+$CheckBox68.Checked = $true             #Disable Windows Update Service
+$CheckBox69.Checked = $true             #Disable Enterprise App Management Service
+$CheckBox70.Checked = $true             #Disable Hyper-V Services
+$CheckBox71.Checked = $true             #Disable Home Group Listener
+$CheckBox72.Checked = $true             #Disable Net.Tcp Port Sharing Service
+$CheckBox73.Checked = $true             #Disable Routing and Remote Access
+$CheckBox74.Checked = $true             #Disable Internet Connection Sharing (ICS)
+$CheckBox75.Checked = $true             #Disable Superfetch (A must for SSD drives, but good to do in general)
+#Scheduled Tasks
+$CheckBox86.Checked = $true             #Disable Smart Screen Specific
+$CheckBox87.Checked = $false            #Disable Microsoft Compatibility Appraiser
+$CheckBox88.Checked = $false            #Disable Programm Data Updater
+$CheckBox89.Checked = $false            #Disable Startup App Task
+$CheckBox90.Checked = $true             #Disable Proxy
+$CheckBox91.Checked = $false            #Disable Consolidator
+$CheckBox92.Checked = $false            #Disable Kernel Ceip Task
+$CheckBox93.Checked = $false            #Disable USB Ceip
+$CheckBox94.Checked = $true             #Disable Microsoft Windows Disk Diagnostic Data Collector
+$CheckBox95.Checked = $true             #Disable File History (maintenance mode)
+$CheckBox96.Checked = $true             #Disable WinSAT
+$CheckBox97.Checked = $false            #Disable Gather Network Info
+$CheckBox98.Checked = $true             #Disable Sqm-Tasks
+$CheckBox99.Checked = $true             #Disable Force Syncronize Time
+$CheckBox100.Checked = $true            #Disable Time Syncronizaion
+$CheckBox101.Checked = $true            #Disable Queue Reporting
+$CheckBox102.Checked = $true            #Disable Automatic App Update
+
+#Windows Default Apps
+$CheckBox80.Checked = $true             #Remove 3D-Viewer"
+$CheckBox103.Checked = $true            #Remove Bing"
+$CheckBox104.Checked = $true            #Remove Zune (Groove Music)"
+$CheckBox105.Checked = $false           #Remove Photo Viewer"
+$CheckBox106.Checked = $true            #Remove Windows Community (Update 1903)"
+$CheckBox107.Checked = $true            #Remove Solitare"
+$CheckBox108.Checked = $true            #Remove Phone"
+$CheckBox109.Checked = $true            #Remove Soundrecorder"
+$CheckBox110.Checked = $true            #Remove Camera App"
+$CheckBox111.Checked = $true            #Remove Microsoft People"
+$CheckBox112.Checked = $true            #Remove Office Live"
+$CheckBox113.Checked = $true           #Remove XBox"
+$CheckBox79.Checked =  $true           #Disable/Remove OneDrive"
+
+#Misc
+$CheckBox81.Checked = $true             #Disable Windows Error Recovery on Startup
+$CheckBox82.Checked = $true             #Internet Explorer 11 Tweaks
+$CheckBox83.Checked = $true             #Libary Tweaks
+$CheckBox84.Checked = $true             #Windows Update Tweaks
+$CheckBox85.Checked = $true             #Windows Defender Tweaks
+$CheckBox56.Checked = $true             #Blocking Telemetry Servers
+$CheckBox78.Checked = $true             #Blocking more Microsoft Servers
+$CheckBox114.Checked = $true            #Mouse Acceleration Fix
+
+
+
+Write-Output "Save Energy"
