97de157fecebcc7f8c6552d54438b7dd8ca9b777
MinersWin <minerswin@minerswin.de>
2020-01-05 19:06:04 +0100

    Update Update.ps1

--- Update.ps1
+++ Update.ps1
@@ -1,7 +1,1 @@
-$GitHub = "https://github.com/MinersWin/TGF-Tuning-Pack-4.0/archive/master.zip"
-$Path = Get-Location
-$Path
-wget $GitHub -OutFile 'Update.zip'
-Expand-Archive .\Update.zip -DestinationPath .\
-Start-Process powershell .\GUI.ps1
-exit
+explorer https://tuning-pack.de
