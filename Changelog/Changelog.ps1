1547d26a9cfb7330b2d59dd998a83f88e0b371a1
MinersWin <minerswin@minerswin.de>
2020-02-15 17:11:08 +0100

    Update Changelog.ps1

--- Changelog/Changelog.ps1
+++ Changelog/Changelog.ps1
@@ -1,6 +1,11 @@
 Add-Type -AssemblyName System.Windows.Forms
 . (Join-Path $PSScriptRoot 'Changelog.designer.ps1')
 $ButtonChangelog.Add_Click{($FormChangelog.Close())}
+$bitmap = [System.Drawing.Bitmap]::FromFile($Config.Application.Icon)
+$bitmap.MakeTransparent()
+$hicon = $bitmap.GetHicon()
+$FormChangelog.Icon = [system.drawing.icon]::FromHandle($hicon)
+$FormChangelog.Text = "TGF Tuning Pack 4.1-beta.5 16.02.2020"
 $Label1.Text="
 16.02.2020
 v.4.1-beta.5
