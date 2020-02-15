25148aa9ae5f2a6133cc759501c41a58237959e5
MinersWin <minerswin@minerswin.de>
2020-02-15 17:43:51 +0100

    Create SendMail.ps1

---
+++ Scripts/PowerShell/SendMail.ps1
@@ -0,0 +1,16 @@
+﻿
+function SendMail {
+	param($Subject, $Body)
+    $msg = new-object Net.Mail.MailMessage
+    $smtp = new-object Net.Mail.SmtpClient($smtpServer)
+    $smtp.EnableSsl = $true
+    $msg.From = "$email" 
+    $msg.To.Add("$email")
+    $msg.IsBodyHTML = $true 
+    $msg.Subject = $Subject
+    $msg.Body = $Body 
+    $SMTP.Credentials = New-Object System.Net.NetworkCredential("$email", "$pass");
+    $smtp.Send($msg)
+}
+
+
