a11709c51c198cabc86ce381ffef758a6ddded45
MinersWin <minerswin@minerswin.de>
2020-02-15 17:44:31 +0100

    Create IpDns.ps1

---
+++ Scripts/PowerShell/IpDns.ps1
@@ -0,0 +1,19 @@
+﻿function SetDns{
+    param(
+        $DnsSettings,
+        $Adapters
+    )
+
+    foreach($Adapter in $Adapters){
+        
+        $DnsInfo = Get-DNSClientServerAddress -InterfaceAlias $Adapter -AddressFamily IPv4
+        
+        if($DnsInfo.ServerAddresses -NotMatch $DnsSettings){
+
+            Set-DNSClientServerAddress -InterfaceAlias $Adapter –ServerAddresses $DnsSettings
+
+        }
+        
+    }
+    
+}
