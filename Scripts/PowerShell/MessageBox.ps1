1a5cd6a6bb687a15d35bfd19eddfb48cc6a0fb57
MinersWin <minerswin@minerswin.de>
2020-02-15 17:44:21 +0100

    Create MessageBox.ps1

---
+++ Scripts/PowerShell/MessageBox.ps1
@@ -0,0 +1,9 @@
+﻿function SimpleMessageBox{
+    Param
+    (
+        $MessageBoxInfo
+    )
+
+    $wshell = New-Object -ComObject Wscript.Shell
+    $wshell.Popup("$MessageBoxInfo",0,"Done",0x1)
+}
