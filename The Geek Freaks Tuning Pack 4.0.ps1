740417a9228f51d7e1434a8cd67f9e209df1b28d
MinersWin <minerswin@minerswin.de>
2020-06-23 12:03:53 +0200

    Added catch to only start the TuningPack if the necessary Files exists

--- The Geek Freaks Tuning Pack 4.0.ps1
+++ The Geek Freaks Tuning Pack 4.0.ps1
@@ -1,1 +1,8 @@
-﻿.\GUI.ps1
+﻿$GUIFileExists = Test-Path ./GUI.ps1
+Write-Host "Wurde das TuningPack ordnungsgemäß entpackt? '$($GUIFileExists)"
+if ($GUIFileExists){
+    .\GUI.ps1
+} else {
+    Write-Host "FEHLER"
+    [System.Windows.Forms.MessageBox]::Show("Du musst das TuningPack vor dem Ausführen entpacken. Am besten einen neuen Ordner auf dem Desktop erstellen und den Inhalt der .Zip datei hineinziehen.")
+}
