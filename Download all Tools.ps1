3c3806268f22a0183ed23e632cc555a86a2edce6
MinersWin <minerswin@minerswin.de>
2020-11-18 11:34:09 +0100

    Removed Old Tool Download

--- Download all Tools.ps1
+++
@@ -1,6 +0,0 @@
-if ($Internet){
-    curl -o Tools.zip root3.minerswin.de/TGF/Tools.zip -UseBasicParsing
-    Expand-Archive .\Tools.zip -DestinationPath .\
-} else {
-    Write-Host "Keine Internetverbindung möglich. Bitte zu einem späteren Zeitpunkt erneut versuchen."
-}
