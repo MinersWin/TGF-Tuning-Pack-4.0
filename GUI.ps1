06ed0266c782fa6a5520bfcd8cfabc5431af0ac3
MinersWin <minerswin@minerswin.de>
2020-12-02 10:02:06 +0100

    Moved Legacy Tools Code, Added Shoplink

--- GUI.ps1
+++ GUI.ps1
@@ -1395,7 +1395,7 @@ function TWEAK_THE_SHIT{
 }
 
 #Templates 
-$Button1.Add_Click{([System.Windows.Forms.MessageBox]::Show("In Version 5!","TGF Tuning Pack 4.2",1))}
+$Button1.Add_Click{($ShopLink = [System.Windows.Forms.MessageBox]::Show("In Version 5 (or 4.5 PRO)`nhttps://shop.thegeekfreaks.de","TGF Tuning Pack 4.2",1); if ($ShopLink -eq "Cancel"){}else{explorer https://shop.thegeekfreaks.de})}
 $Button2.Add_Click{(Apply-Template)}
 
 function Apply-Template{
@@ -1416,280 +1416,8 @@ function Apply-Template{
     }
 }
 
-
-######################################################################################################################################################################################
-### All the Tools ###
-######################################################################################################################################################################################
-#Process Killer
-function Test_Kill_Process{
-$Kill_Process = Test-Path .\Scripts\ProcessKiller-Tuning-Pack.bat
-if ($Kill_Process){
-    $Button3.Add_Click{(Kill-Process)}
-    $Button3.ForeColor = 'Green'
-} else {
-    $Button3.Add_Click{(Download_Kill-Process)}
-    $Button3.ForeColor = 'RED'
-}
-}
-function Kill-Process{
-    [System.Windows.Forms.MessageBox]::Show("Prozesse werden beendet","TGF Tuning Pack 4.1",1)
-    Write-Host "Prozesse werden beendet."
-    & '.\Scripts\ProcessKiller-Tuning-Pack.bat'
-    Write-Host "Die Prozesse wurden beendet"
-    Test_Kill_Process
-}
-function Download_Kill-Process{
-    Set-Location .\Scripts\
-    wget 'http://download.tuning-pack.de/TGF/Scripts/ProcessKiller-Tuning-Pack.bat' -OutFile 'ProcessKiller-Tuning-Pack.bat' -UseBasicParsing
-    Set-Location .\..\
-    Test_Kill_Process
-}
-#Internet Explorer Cleanup
-function Test_IE_Clean{
-    $IE_Clean = Test-Path '.\Tools\IE Cleanup\Clear-IECachedData.ps1'
-    if ($IE_Clean){
-        $Button4.Add_Click{(IE_Clean)}
-        $Button4.ForeColor = 'Green'
-    } else {
-        $Button4.Add_Click{(Download_IE_Clean)}
-        $Button4.ForeColor = 'RED'
-    }
-    }
-    function IE_Clean{
-        & '.\Tools\IE Cleanup\Clear-IECachedData.ps1'
-    }
-    function Download_IE_Clean{
-        Set-Location .\Tools\
-        wget 'http://download.tuning-pack.de/TGF/Tools/IE%20Cleanup.zip' -OutFile 'IE_Cleanup.zip' -UseBasicParsing
-        Expand-Archive .\IE_Cleanup.zip -DestinationPath .\
-        Set-Location .\..\
-        Test_IE_Clean
-    }
-#CCleaner
-function Test_CCleaner{
-    $IE_Clean = Test-Path '.\Tools\CCleaner\CCleaner.exe'
-    if ($IE_Clean){
-        $Button20.Add_Click{(CCleaner)}
-        $Button20.ForeColor = 'Green'
-    } else {
-        $Button20.Add_Click{(Download_CCleaner)}
-        $Button20.ForeColor = 'RED'
-    }
-    }
-    function CCleaner{
-        .\Tools\CCleaner\CCleaner.exe
-    }
-    function Download_CCleaner{
-        Set-Location .\Tools\
-        wget 'http://download.tuning-pack.de/TGF/Tools/CCleaner.zip' -OutFile 'CCleaner.zip' -UseBasicParsing
-        Expand-Archive .\CCleaner.zip -DestinationPath .\
-        Set-Location .\..\
-        Test_CCleaner
-    }
-#BleachBit
-function Test_BleachBit{
-    $IE_Clean = Test-Path '.\Tools\BleachBit\bleachbit.exe'
-    if ($IE_Clean){
-        $Button21.Add_Click{(BleachBit)}
-        $Button21.ForeColor = 'Green'
-    } else {
-        $Button21.Add_Click{(Download_BleachBit)}
-        $Button21.ForeColor = 'RED'
-    }
-    }
-    function BleachBit{
-        .\Tools\BleachBit\bleachbit.exe
-    }
-    function Download_BleachBit{
-        Set-Location .\Tools\
-        wget 'http://download.tuning-pack.de/TGF/Tools/BleachBit.zip' -OutFile 'BleachBit.zip' -UseBasicParsing
-        Expand-Archive .\BleachBit.zip -DestinationPath .\
-        Set-Location .\..\
-        Test_BleachBit
-    }
-#Temp File Cleanup
-function Test_TFC{
-    $IE_Clean = Test-Path '.\Tools\Temp File Cleanup\Cleanup.ps1'
-    if ($IE_Clean){
-        $Button22.Add_Click{(TFC)}
-        $Button22.ForeColor = 'Green'
-    } else {
-        $Button22.Add_Click{(Download_TFC)}
-        $Button22.ForeColor = 'RED'
-    }
-    }
-    function TFC{
-        & '.\Tools\Temp File Cleanup\Cleanup.ps1'
-    }
-    function Download_TFC{
-        Set-Location .\Tools\
-        wget 'http://download.tuning-pack.de/TGF/Tools/Temp%20File%20Cleanup.zip' -OutFile 'TempFileCleanup.zip' -UseBasicParsing
-        Expand-Archive .\TempFileCleanup.zip -DestinationPath .\
-        rm TempFileCleanup.zip
-        Set-Location .\..\
-        Test_TFC
-    }
-#USB Device Cleanup
-function Test_USBDeviceCleanup{
-    $IE_Clean = Test-Path '.\Tools\Drive Cleanup\DriveCleanup.exe'
-    if ($IE_Clean){
-        $Button23.Add_Click{(USBDeviceCleanup)}
-        $Button23.ForeColor = 'Green'
-    } else {
-        $Button23.Add_Click{(Download_USBDeviceCleanup)}
-        $Button23.ForeColor = 'RED'
-    }
-    }
-    function USBDeviceCleanup{
-        & '.\Tools\Drive Cleanup\DriveCleanup.exe'
-    }
-    function Download_USBDeviceCleanup{
-        Set-Location .\Tools\
-        wget 'http://download.tuning-pack.de/TGF/Tools/Drive%20Cleanup.zip' -OutFile 'DriveCleanup.zip' -UseBasicParsing
-        Expand-Archive .\DriveCleanup.zip -DestinationPath .\
-        rm DriveCleanup.zip
-        Set-Location .\..\
-        Test_USBDeviceCleanup
-    }
-
-#Cleanup Duplicate Downloads
-###FIXME###
-
-#$Button24.Add_Click{([System.Windows.Forms.MessageBox]::Show("WIP","TGF Tuning Pack 4.1"1))}
-
-#Clear Windows event logs
-$Button25.ForeColor = 'GREEN'
-$Button25.Add_Click{(Clear-EventLog Application,Security,System)}
-#Clear Windows Update Cache
-function Test_Clear-UpdateCache{
-    $IE_Clean = Test-Path '.\Tools\Clear Update Cache\Erase_Cache.bat'
-    if ($IE_Clean){
-        $Button26.Add_Click{(Clear-UpdateCache)}
-        $Button26.ForeColor = 'Green'
-    } else {
-        $Button26.Add_Click{(Download_Clear-UpdateCache)}
-        $Button26.ForeColor = 'RED'
-    }
-    }
-    function Clear-UpdateCache{
-        & '.\Tools\Clear Update Cache\Erase_Cache.bat'
-    }
-    function Download_Clear-UpdateCache{
-        Set-Location .\Tools\
-        wget 'http://download.tuning-pack.de/TGF/Tools/Clear%20Update%20Cache.zip' -OutFile 'ClearUpdateCache.zip' -UseBasicParsing
-        Expand-Archive .\ClearUpdateCache.zip -DestinationPath .\
-        rm ClearUpdateCache.zip
-        Set-Location .\..\
-        Test_Clear-UpdateCache
-    }
-#Clear CryptNet SSL Cache
-$Button7.Add_Click{(Clear-SSL)}
-function Clear-SSL{
-    certutil -URLcache * delete
-}
-#Malwarebytes
-$Button8.Add_Click{(Malwarebytes)}
-function Malwarebytes{
-    .\Tools\Malwarebytes\mb3-setup-consumer-3.8.3.2965-1.0.613-1.0.11878.exe
-}
-#Kaspersky Virus Removal Tool
-$Button9.Add_Click{(KVRT)}
-function KVRT{
-    .\Tools\KVRT\KVRT.exe
-}
-#Sophos Virus Removal Tool
-$Button10.Add_Click{(Sophos)}
-function Sophos{
-    & '.\tools\Sophos Virus Removal Tool\Sophos Virus Removal Tool.exe'
-}
-#MSI installer Cleanup
-$Button11.Add_Click{(Installer-Cleanup)}
-function Installer-Cleanup{
-    [System.Windows.Forms.MessageBox]::Show("Diese funktion ist momentan nicht verfügbar, Microsoft hat dieses Feature eingestellt. Ich arbeite momentan an einer Alternative","TGF Tuning Pack 4.1 by MinersWin",1)
-}
-#System File Checker
-$Button12.Add_Click{(System-File-Checker)}
-function System-File-Checker{
-    [System.Windows.Forms.MessageBox]::Show("Es wird sich gleich ein Konsolenfenster öffnen, in welchem ein Windows Image heruntergeladen wird, mit welchem das System auf Fehler geprüft wird. Das Image wird nach Abschluss des Vorgangs wieder gelöscht. Zum Bestätigen OK drücken","TGF Tuning Pack 4.1 by MinersWin",1)    
-    & '.\Tools\System File Checker\Check.bat'
-}
-#DISM image check and repair
-$Button27.Add_Click{(System-File-Checker)}
-#CheckDisk
-$button28.Add_Click{(start cmd.exe 'chkdsk C: /f /r')}
-#Network Repair
-$Button30.Add_Click{(Network-Repair)}
-function Network-Repair{
-    ipconfig /flushdns
-    netsh interface ip delete arpcache
-    netsh winsock reset catalog
-}
-#Page File Reset
-$Button32.Add_Click{(Page-File-Reset)}
-function Page-File-Reset{
-    C: computersystem where name="%computername%" set AutomaticManagedPagefile=True
-}
-#Defraggler
-$Button33.Add_Click{(Defraggler-Start)}
-function Defraggler-Start{
-    .\Tools\Defraggler\Defraggler64.exe
-}
-$Button34.Add_Click{(ADSSPy)}
-function ADSSPy{
-    .\Tools\ADSSpy\ADSSpy.exe
-}
-#Adwcleaner
-$Button35.Add_Click{(ADWCleaner)}
-function ADWCleaner{
-    .\Tools\AdwCleaner\adwcleaner_7.4.1.exe
-}
-#aswMBR
-$Button36.Add_Click{(aswMBR)}
-function aswMBR{
-    .\Tools\aswMBR\aswmbr.exe
-}
-#autorun
-$button37.Add_Click{(autorun)}
-function autorun{
-    .\Tools\autorun\Autoruns64.exe
-}
-#PCHunter
-$Button38.Add_Click{(PCHunter)}
-function PCHunter{
-    .\Tools\PCHunter\PCHunter64.exe
-}
-#Net Adapter Repair
-$Button40.Add_Click{(Net-Adapter-Repair)}
-function Net-Adapter-Repair{
-    .\tools\NetAdapterRepair\NetAdapterRepair1.2.exe
-}
-#ParkControl
-$Button29.Add_Click{(Park-Control)}
-function Park-Control{
-    .\Tools\ParkControl\ParkControl.exe
-}
-#System Recovery
-$Button39.Add_Click{(System-Recovery)}
-function System-Recovery{
-    rstrui.exe
-}
-#Reboot to Recovery
-$Button42.Add_Click{(Reboot-Recovery)}
-function Reboot-Recovery{
-    shutdown /r /o /f /t 00
-}
-
-function Refresh_Tools{
-#Test_Kill_Process
-#Test_IE_Clean
-#Test_CCleaner
-#Test_BleachBit
-#Test_TFC
-#Test_USBDeviceCleanup
-#Test_Clear-UpdateCache
-}
-Refresh_Tools
-
+#Run script to Define the Tool Variables
+.\Tools.ps1
 
 
 #About Button
@@ -1707,7 +1435,7 @@ function About {
     $aboutForm.ControlBox    = $false
     $aboutForm.ShowInTaskBar = $false
     $aboutForm.StartPosition = "CenterParent"
-    $aboutForm.Text          = "The Geek Freaks Tuning Pack 4.2"
+    $aboutForm.Text          = "The Geek Freaks Tuning Pack 4.3"
     $aboutForm.Add_Load($aboutForm_Load)
     # About PictureBox
     $icon = [System.Drawing.Bitmap]::FromFile('.\Images\favicon.ico')
@@ -1720,13 +1448,13 @@ function About {
     $aboutFormNameLabel.Font     = New-Object Drawing.Font("Microsoft Sans Serif", 9, [System.Drawing.FontStyle]::Bold)
     $aboutFormNameLabel.Location = "110, 20"
     $aboutFormNameLabel.Size     = "200, 18"
-    $aboutFormNameLabel.Text     = "The Geek Freaks Tuning Pack 4.2"
+    $aboutFormNameLabel.Text     = "The Geek Freaks Tuning Pack 4.3"
     $aboutForm.Controls.Add($aboutFormNameLabel)
     # About Text Label
     $aboutFormText.Location = "100, 40"
     $aboutFormText.Size     = "300, 30"
     $aboutFormText.Text     = "          Created by MinersWin `n`r https://www.thegeekfreaks.de"
-    $aboutFormText.Add_Click{(explorer https://thegeekfreaks.de)}
+    $aboutFormText.Add_Click{(explorer https://thegeekfreaks.de); explorer https://youtube.com/minerswin}
     $aboutForm.Controls.Add($aboutFormText)
     # About Exit Button
     $aboutFormExit.Location = "135, 70"
